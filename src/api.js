bb40d89d15a487cd3dc8c791cf79a6b64d0cf17d
Gabija <97754554+gabijonsis@users.noreply.github.com>
2022-11-01 02:49:51 +0200

    First react try

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'dd666212a7mshf0905ad1a6ec65fp181fb0jsned0fd389212c',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "82c39c82998ad0bcd5f4ca442178ea53";
